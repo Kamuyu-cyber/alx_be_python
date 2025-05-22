eaa74cd4a647df1ec217451b8b1ba28a26c5c81d
Kamuyu-cyber <kamuyumax@gmail.com>
2025-05-22 01:52:41 -0700

    arithmetic

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+number1 = 16
+number2 = 10
+# Calculate addition
+sum_result = number1 + number2
+print(f"Addition of {number1} and {number2} is {sum_result}")
+
+# Calculate subtraction
+difference = number1 - number2
+print(f"Subtraction of {number1} and {number2} is {difference}")
+
+# Calculate multiplication
+product = number1 * number2
+print(f"Multiplication of {number1} and {number2} is {product}")
