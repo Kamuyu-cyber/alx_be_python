9ca3e08d093a8f33ccd013c05154e6050e709086
Kamuyu-cyber <your.kamuyumax@gmail.com>
2025-06-08 18:22:52 +0300

    Add Data_structures and lambda.py

---
+++ python_introduction/lambda.py
@@ -0,0 +1,38 @@
+# def greet (name):
+#     print("Hello",name)
+# greet("Alice")
+
+
+
+
+# def user_info(name, age):
+#     print(f"Name: {name}")
+#     if age:
+#         print(f"Age: {age}")
+
+
+# user_info(name="Frank", age= 28)
+
+
+
+# def great (name):
+#     print(f"Hello",name)
+# great("frank")
+
+
+# def c_area (l, w):
+#     area = l * w
+#     return area
+
+# print(c_area(10, 5))
+
+
+
+
+def check_numbebr_even(number):
+    if number % 2 == 0:
+        print(f"{number} is even")
+    else:
+        print(f"{number} is odd")
+
+check_numbebr_even(27999)
